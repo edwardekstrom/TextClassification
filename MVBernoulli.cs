c4c5c4f4488e076012ec992fadbc82179edecc01
link834 <link834@gmail.com>
2014-05-24 15:10:51 -0600

    added the MVBernoulli class

---
+++ MVBernoulli.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MNBClassifier
+{
+    // this code was copied from the Multinomial Code and should be updated as needed
+    class MVBernoulli
+    {
+        private Dictionary<string, int> vocabOccur;
+        private string label;
+        private int numTermsInDoc;
+
+        public MVBernoulli(Dictionary<string, int> vocabOccur, string lbl)
+        {
+            this.vocabOccur = vocabOccur;
+            label = lbl;
+            numTermsInDoc = 0;
+            foreach(string key in vocabOccur.Keys)
+            {
+                numTermsInDoc += vocabOccur[key];
+            }
+        }
+
+        public void printVocabCounts()
+        {
+            foreach(string word in vocabOccur.Keys)
+            {
+                Console.WriteLine("\t" + word + "\t\t" + vocabOccur[word]);
+            }
+        }
+
+        public Dictionary<string, int> VocabOccur
+        {
+            get { return vocabOccur; }
+        }
+        
+        public int NumTermsInDoc
+        {
+            get { return numTermsInDoc; }
+        }
+
+        public string Label
+        {
+            get { return label; }
+            set { label = value; }
+        }
+    }
+}
